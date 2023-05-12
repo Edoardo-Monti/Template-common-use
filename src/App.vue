3a8a205aa1bf3153abb2f04307479f5421966394
Your Namede <edoardo.monti.bo@gmail.com>
2023-05-12 13:49:34 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script >
-
+import {store} from './store'
+import MainComp from './components/MainComp.vue';
 
 export default{
   name: 'App',
   components: {
-    
+    MainComp,
+  },
+  data(){
+    return{
+      store
+    }
   }
 }
 
@@ -12,10 +18,16 @@ export default{
 
 <template>
   <div>
-    <h1 class="text-center">ciao</h1>
+    <h1 class="animate__animated animate__rubberBand">ciao</h1>
+    
+    <MainComp/>
   </div>
 </template>
 
 <style lang="scss">
-@use "./style/main.scss"
+@use "./style/main.scss";
+.animate__animated.animate__rubberBand{
+
+    animation-delay: 2s;
+}
 </style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+  name: "MainComp"
+}
+</script>
+
+<template>
+  <h1 class="animate__animated animate__rubberBand">ciewnceirc</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
